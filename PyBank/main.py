4f172f10fb00f3c555058783f877a88e8a1f1866
dittu <37671342+dittu276@users.noreply.github.com>
2018-06-27 19:10:18 -0700

    almost - get greatest month

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,33 +1,45 @@
 import os
 import csv
-csvpath = os.path.join('..','..','Instructions', 'PyBank','Resources','budget_data.csv')
+csvpath = os.path.join('..','..','Instructions','PyBank','Resources','budget_data.csv')
 with open(csvpath, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     csv_header = next(csvreader)
     total_months=0
     total=0
     revenue=[]
+    row_number="first_row"
+    change=0
+    previous_row=0
+    change_list=[]
     for row in csvreader:
         total_months=total_months+1
         total=total+int(row[1])
         revenue.append(int(row[1]))
+        if row_number=="first_row":
+            previous_row=int(row[1])
+            row_number="next_row"
+        else:
+            change=(int(row[1])-previous_row)
+            change_list.append(int(change))
+            previous_row=int(row[1])
+            row_number=="next_row"     
     greatest_increase=max(revenue)
     greatest_decrease=min(revenue)
-    average_change=round(total/total_months,2)
-    print('Financial Analysis \n')
+    csvfile.seek(0)
+    for values in csvreader:
+        if values[1]==greatest_increase:
+            greatest_increase_month=values[0]
+        if values[1]==greatest_decrease:
+            greatest_decrease_month=values[0]
+    change_sum=sum(change_list)
+    average_change=round(change_sum/len(change_list),2)
+    print('\nFinancial Analysis \n')
     print('------------------------------')
     print('Total Months: '+str(total_months))
     print('Total: $'+str(total))
     print('Average Change: $'+str(average_change))
-    for rows in csvreader:
-        if greatest_increase==rows[1]:
-            greatest_increase_month=rows[0]
-        if greatest_decrease==rows[1]:
-            greatest_decrease_month=rows[0]      
-    print('Greatest Increase in Profits: '+greatest_increase_month +' ($'+str(greatest_increase)+')')
-    print('Greatest Decrease in Profits: '+greatest_decrease_month +' ($'+str(greatest_decrease)+')')
+    print(greatest_increase_month)
+    print(greatest_decrease_month)
 
 
 
-
-    
