93057bc7c6354aa7628739116c1b52116e0b314b
dittu <37671342+dittu276@users.noreply.github.com>
2018-06-30 02:35:27 -0700

    export to text file remaining

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,20 +1,25 @@
 import os
 import csv
+#path
 csvpath = os.path.join('..','..','Instructions','PyBank','Resources','budget_data.csv')
+#read csv file
 with open(csvpath, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
+#read header
     csv_header = next(csvreader)
+#declare variables
     total_months=0
     total=0
-    revenue=[]
     row_number="first_row"
     change=0
     previous_row=0
     change_list=[]
+    month=[]
+#find total months. total revenue and change between months
     for row in csvreader:
         total_months=total_months+1
         total=total+int(row[1])
-        revenue.append(int(row[1]))
+        month.append(str(row[0]))
         if row_number=="first_row":
             previous_row=int(row[1])
             row_number="next_row"
@@ -23,23 +28,25 @@ with open(csvpath, newline='') as csvfile:
             change_list.append(int(change))
             previous_row=int(row[1])
             row_number=="next_row"     
-    greatest_increase=max(revenue)
-    greatest_decrease=min(revenue)
-    csvfile.seek(0)
-    for values in csvreader:
-        if values[1]==greatest_increase:
-            greatest_increase_month=values[0]
-        if values[1]==greatest_decrease:
-            greatest_decrease_month=values[0]
+#find greatest increase and decrease and find index for those in the change list
+    greatest_increase=max(change_list)
+    max_index=change_list.index(greatest_increase)
+    greatest_decrease=min(change_list)
+    min_index=change_list.index(greatest_decrease)
+#find the max and min month in the month list
+    max_month=month[max_index+1]
+    min_month=month[min_index+1]
+#find average change
     change_sum=sum(change_list)
     average_change=round(change_sum/len(change_list),2)
+#print results to terminal
     print('\nFinancial Analysis \n')
     print('------------------------------')
     print('Total Months: '+str(total_months))
     print('Total: $'+str(total))
     print('Average Change: $'+str(average_change))
-    print(greatest_increase_month)
-    print(greatest_decrease_month)
+    print('Greatest Increase in Profits:'+' '+str(max_month)+ ' ('+str(greatest_increase)+')')
+    print('Greatest Decrease in Profits:'+' '+str(min_month)+ ' ('+str(greatest_decrease)+')')
 
 
 
